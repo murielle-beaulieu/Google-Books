b7d999a06607551b90b79c63e06e0930a31d0ba6
murielle-beaulieu <murielle.beaulieu19@gmail.com>
2025-01-11 12:42:25 +1100

    Adding Header component

--- src/components/BookCard/BookCard.jsx
+++ src/components/BookCard/BookCard.jsx
@@ -1,14 +1,15 @@
-import { getBooksData } from '../../data/books-data';
 import classes from "./BookCard.module.scss";
 
 const BookCard = ({bookData}) => {
 
-  return <div className={classes.bookcard}>
-    <h3>{bookData.volumeInfo.title}</h3>
-    <p>by {bookData.volumeInfo.authors}</p>
-    <p>{bookData.volumeInfo.publishedDate}</p>
-    <img src={bookData.volumeInfo.imageLinks.thumbnail} alt={bookData.volumeInfo.title}></img>
-  </div>
+  return (
+    <div className={classes.bookcard}>
+      <h3>{bookData.volumeInfo.title}</h3>
+      <p>by {bookData.volumeInfo.authors}</p>
+      <p>{bookData.volumeInfo.publishedDate}</p>
+      <img src={bookData.volumeInfo.imageLinks.thumbnail} alt={bookData.volumeInfo.title}></img>
+    </div>
+  )
 }
 
 export default BookCard;
